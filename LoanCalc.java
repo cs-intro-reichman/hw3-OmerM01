740012b41322bf6477f7dd4e6cb227ff8385ac53
OmerM01 <omerwork00@gmail.com>
2024-11-24 12:33:40 +0200

    HW_18

--- LoanCalc.java
+++ LoanCalc.java
@@ -61,6 +61,7 @@ public class LoanCalc {
 		double H = bruteForceSolver(loan, rate, n, epsilon) + 1;
 		double g = (L + H)/2;
 		iterationCounter = 0;
+		
 		while((H-L) > epsilon){
 			if(endBalance(loan, rate, n, g) * endBalance(loan, rate, n, L) > 0){
 				L = g;
